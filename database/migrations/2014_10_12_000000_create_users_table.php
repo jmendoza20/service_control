3de62c04ec3f76ebfb24dacd0d339a2ca1befb5e
jmendoza20 <ricardoww10@hotmail.com>
2019-02-06 16:13:28 -0600

    Add files via upload

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::defaultStringLength(191);
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id')->unique();;
+            $table->string('name');
+            $table->integer('age');
+            $table->string('gender');
+            $table->string('rol')->references('rol')->on('roles');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+
+            //$table->foreign('rol')->references('rol')->on('roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
